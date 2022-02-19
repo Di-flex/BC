2cf99858dbc2f51098f6dd46baf158d3a1519a7b
Ada <gubertpol0@gmail.com>
2022-02-18 23:04:52 -0800

    Heal

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonFight.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonFight.js
@@ -598,7 +598,7 @@ function KinkyDungeonBulletHit(b, born, outOfTime, outOfRange) {
 	} else if (b.bullet.hit == "heal") {
 		KinkyDungeonBullets.push({born: born, time:b.bullet.spell.lifetime, x:b.x, y:b.y, vx:0, vy:0, xx:b.x, yy:b.y, spriteID:b.bullet.name+"Hit" + CommonTime(),
 			bullet:{spell:b.bullet.spell, damage: {damage:(b.bullet.spell.aoedamage) ? b.bullet.spell.aoedamage : b.bullet.spell.power, type:b.bullet.spell.damage, time:b.bullet.spell.time}, aoe: b.bullet.spell.aoe, lifetime: b.bullet.spell.lifetime, passthrough:true, name:b.bullet.name+"Hit", width:b.bullet.width, height:b.bullet.height}});
-		if (b.bullet.spell && (b.bullet.spell.playerEffect || b.bullet.playerEffect) && KinkyDungeonPlayerEntity.x == b.x && KinkyDungeonPlayerEntity.y == b.y) {
+		if (b.bullet.spell && (b.bullet.spell.playerEffect || b.bullet.playerEffect) && KDistEuclidean(b.x - KinkyDungeonPlayerEntity.x, b.y - KinkyDungeonPlayerEntity.y) < b.bullet.spell.aoe) {
 			KinkyDungeonPlayerEffect(b.bullet.damage.type, b.bullet.playerEffect ? b.bullet.playerEffect : b.bullet.spell.playerEffect, b.bullet.spell);
 		}
 		for (let L = 0; L < KinkyDungeonEntities.length; L++) {
@@ -728,7 +728,12 @@ function KinkyDungeonBulletsCheckCollision(bullet, AoE) {
 				for (let L = 0; L < KinkyDungeonEntities.length; L++) {
 					let enemy = KinkyDungeonEntities[L];
 					if ((!bullet.bullet.spell || (!bullet.bullet.spell.enemySpell && !enemy.Enemy.allied) || (!bullet.bullet.spell.allySpell && enemy.Enemy.allied)) && bullet.bullet.aoe >= Math.sqrt((enemy.x - bullet.x) * (enemy.x - bullet.x) + (enemy.y - bullet.y) * (enemy.y - bullet.y))) {
-						KinkyDungeonDamageEnemy(enemy, bullet.bullet.damage, true, nomsg, bullet.bullet.spell, bullet);
+						if (bullet.bullet.damage.type == "heal") {
+							let origHP = enemy.hp;
+							enemy.hp = Math.min(enemy.hp + bullet.bullet.spell.power, enemy.Enemy.maxhp);
+							KinkyDungeonSendFloater(enemy, `+${Math.round((enemy.hp - origHP) * 10)}`, "#ffaa00", 3);
+						} else
+							KinkyDungeonDamageEnemy(enemy, bullet.bullet.damage, true, nomsg, bullet.bullet.spell, bullet);
 						nomsg = true;
 					}
 				}
@@ -741,7 +746,12 @@ function KinkyDungeonBulletsCheckCollision(bullet, AoE) {
 			for (let L = 0; L < KinkyDungeonEntities.length; L++) {
 				let enemy = KinkyDungeonEntities[L];
 				if ((!bullet.bullet.spell || (!bullet.bullet.spell.enemySpell && !enemy.Enemy.allied) || (!bullet.bullet.spell.allySpell && enemy.Enemy.allied)) && enemy.x == bullet.x && enemy.y == bullet.y) {
-					KinkyDungeonDamageEnemy(enemy, bullet.bullet.damage, true, bullet.bullet.NoMsg, bullet.bullet.spell, bullet);
+					if (bullet.bullet.damage.type == "heal") {
+						let origHP = enemy.hp;
+						enemy.hp = Math.min(enemy.hp + bullet.bullet.spell.power, enemy.Enemy.maxhp);
+						KinkyDungeonSendFloater(enemy, `+${Math.round((enemy.hp - origHP) * 10)}`, "#ffaa00", 3);
+					} else
+						KinkyDungeonDamageEnemy(enemy, bullet.bullet.damage, true, bullet.bullet.NoMsg, bullet.bullet.spell, bullet);
 
 					return false;
 				}
