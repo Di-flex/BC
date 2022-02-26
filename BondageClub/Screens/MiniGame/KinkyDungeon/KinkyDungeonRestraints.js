03ebb0425a58afde41a03a41c870c67fd51902b6
Ada <gubertpol0@gmail.com>
2022-02-26 12:30:25 -0800

    Sub changes

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonRestraints.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonRestraints.js
@@ -1656,7 +1656,8 @@ function KinkyDungeonGetRestraint(enemy, Level, Index, Bypass, Lock, RequireStam
 	let restraintWeightTotal = 0;
 	let restraintWeights = [];
 	let cache = KDRestraintsCache.get(enemy.name);
-	let staminaPercent = KinkyDungeonStatStamina / KinkyDungeonStatStaminaMax;
+	let staminaPercent = (Math.max(KinkyDungeonStatStamina / KinkyDungeonStatStaminaMax, 1 - KinkyDungeonStatArousal / KinkyDungeonStatArousalMax))
+		/(1 + (KinkyDungeonGoddessRep.Ghost + 50)/100);
 
 	if (KinkyDungeonSlowLevel > 0) staminaPercent = staminaPercent * (0.5 + 0.5 * Math.min(1, Math.max(0, 1 - KinkyDungeonSlowLevel/3)));
 
