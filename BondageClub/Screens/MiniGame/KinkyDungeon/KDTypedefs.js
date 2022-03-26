5aad58565a8b85df7e2d53f53ee45a89bdfe8ef4
Demopans <gao.jeff3@gmail.com>
2022-03-26 16:04:10 -0400

    quick docs

---
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KDTypedefs.js
@@ -0,0 +1,4 @@
+/**
+ * Typedefs.
+ * @typedef {{name: string, type: string, misc: *}} item Representation of an item, contains only tags. misc can be other parameters such as "lock" or "event"
+ */
