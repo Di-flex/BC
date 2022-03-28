ffd14207434fffe877a569b99e8c40c186282ceb
Ada <gubertpol0@gmail.com>
2022-03-27 18:58:15 -0700

    Comments

--- BondageClub/Screens/MiniGame/KinkyDungeon/KDTypeDefs.ts
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KDTypeDefs.ts
@@ -1,35 +1,37 @@
-// Kinky Dungeon Typedefs
+/** Kinky Dungeon Typedefs*/
 interface item {
-    name?: string, // Name of the item
-    type?: string, // Type of the item
-    events?: any, // Events associated with the item
-    weapon?: KinkyDungeonWeapon, // Item weapon data, if applicable
-    consumable?: any, // Item consumable data, if applicable
-    quantity?: number, // Number of consumables in the inventory
-    outfit?: any, // Outfit data, if applicable
-    looserestraint?: any, // Loose restraint data, if applicable
-    restraint?: any, // Which restraint the item is associated with
-    lock?: string, // Type of lock, Red, Blue, or Gold (potentially more in future)
-    tetherToLeasher?: boolean, // Bool to describe if the item is tethered to the leashing enemy
-    tetherToGuard?: boolean, // Bool to describe if the item is tethered to KinkyDungeonJailGuard()
-    tx?: number, // Location of the tether
-    ty?: number, // Location of the tether
-    tetherLength?: number, // Length of the tether
-    lockTimer?: number, // Used for Gold locks only, determines which floor the lock will release
-    dynamicLink?: string[], // Stores a list of restraint names for the linked item system
-    oldLock?: string[], // Stores linked item locks
-    oldTightness?: number[], // Stores linked item tightness
-    battery?: number, // Vibrator battery level
-    cooldown?: number, // Vibrator cooldown, won't restart vibrrating until this is 0. Ticks down each turn.
-    deny?: number, // Vibrator deny timer, similar to cooldown but independent. Ticks down each turn.
-    pickProgress?: number, // Escape progress tracking
-    struggleProgress?: number, // Escape progress tracking
-    removeProgress?: number, // Escape progress tracking
-    cutProgress?: number, // Escape progress tracking
-    unlockProgress?: number, // Escape progress tracking
-    attempts?: number, // Number of escape attempts, integer
-    tightness?: number, // Can be used to make an item tighter and harder to escape, reduces with each escape attempt
-    trap?: string, // Determines the current trap attached to the restraint
+     /** Name of the item*/
+    name?: string,
+    /** Type of the item*/
+    type?: string,
+    events?: KinkyDungeonEvent[], /** Events associated with the item*/
+    weapon?: KinkyDungeonWeapon, /** Item weapon data, if applicable*/
+    consumable?: any, /** Item consumable data, if applicable*/
+    quantity?: number, /** Number of consumables in the inventory*/
+    outfit?: any, /** Outfit data, if applicable*/
+    looserestraint?: any, /** Loose restraint data, if applicable*/
+    restraint?: any, /** Which restraint the item is associated with*/
+    lock?: string, /** Type of lock, Red, Blue, or Gold (potentially more in future)*/
+    tetherToLeasher?: boolean, /** Bool to describe if the item is tethered to the leashing enemy*/
+    tetherToGuard?: boolean, /** Bool to describe if the item is tethered to KinkyDungeonJailGuard()*/
+    tx?: number, /** Location of the tether*/
+    ty?: number, /** Location of the tether*/
+    tetherLength?: number, /** Length of the tether*/
+    lockTimer?: number, /** Used for Gold locks only, determines which floor the lock will release*/
+    dynamicLink?: string[], /** Stores a list of restraint names for the linked item system*/
+    oldLock?: string[], /** Stores linked item locks*/
+    oldTightness?: number[], /** Stores linked item tightness*/
+    battery?: number, /** Vibrator battery level*/
+    cooldown?: number, /** Vibrator cooldown, won't restart vibrrating until this is 0. Ticks down each turn.*/
+    deny?: number, /** Vibrator deny timer, similar to cooldown but independent. Ticks down each turn.*/
+    pickProgress?: number, /** Escape progress tracking*/
+    struggleProgress?: number, /** Escape progress tracking*/
+    removeProgress?: number, /** Escape progress tracking*/
+    cutProgress?: number, /** Escape progress tracking*/
+    unlockProgress?: number, /** Escape progress tracking*/
+    attempts?: number, /** Number of escape attempts, integer*/
+    tightness?: number, /** Can be used to make an item tighter and harder to escape, reduces with each escape attempt*/
+    trap?: string, /** Determines the current trap attached to the restraint*/
 }
 
 interface KinkyDungeonSave {
@@ -114,4 +116,6 @@ interface KinkyDungeonEvent {
 	time?: number;
 	chance?: number;
 	buff?: any;
+    requireEnergy?: any;
+    energyCost?: any;
 }
