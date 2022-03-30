401bb45e5e98cd18f08c7df4f58530797f879f34
Ada <gubertpol0@gmail.com>
2022-03-29 23:13:06 -0700

    Fix jail leash moving enemmies out of cell

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonJail.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonJail.js
@@ -571,8 +571,8 @@ function KinkyDungeonDefeat() {
 		let enemies = [];
 		for (let e of  KinkyDungeonEntities) {
 			if (!e.summoned && (e.Enemy.tags.has("jail") || e.Enemy.tags.has("jailer"))) {
-				if (e.x < KinkyDungeonJailLeashX) {
-					e.x = KinkyDungeonJailLeashX;
+				if (e.x < KinkyDungeonJailLeashX + 4) {
+					e.x = KinkyDungeonJailLeashX + 4;
 					e.y = KinkyDungeonStartPosition.y;
 				}
 				enemies.push(e);
