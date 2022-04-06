74ca19d9abd2d206f10f2e207134a7a8bf8493b7
Ada <gubertpol0@gmail.com>
2022-04-06 15:56:44 -0700

    fixx

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
@@ -944,7 +944,7 @@ function KinkyDungeonHandleClick() {
 		}
 		if (MouseIn(1275, 930, 375, 64)) {
 			let url = 'https://www.patreon.com/ada18980';
-			KDSendEvent(type);
+			KDSendEvent('patreon');
 			window.open(url, '_blank');
 			return true;
 		}
