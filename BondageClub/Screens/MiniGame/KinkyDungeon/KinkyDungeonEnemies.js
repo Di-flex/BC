c853239190bdb345cbd8ab80d9bb3620ada882d7
Ada <gubertpol0@gmail.com>
2022-05-06 00:16:55 -0700

    Friendship~!

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonEnemies.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonEnemies.js
@@ -553,17 +553,41 @@ function KinkyDungeonEnemyCheckHP(enemy, E) {
 		if (!(enemy.lifetime < 9000)) {
 			if (enemy.playerdmg) {
 				let faction = KDGetFaction(enemy);
-				if (!KinkyDungeonHiddenFactions.includes(faction) && !KDFactionHostile("Player", faction)) {
+				let amount = 0;
+
+				if (!KinkyDungeonHiddenFactions.includes(faction)) {
 					if (enemy.Enemy && enemy.Enemy.tags && enemy.Enemy.tags.has("boss"))
-						KinkyDungeonChangeFactionRep(faction, -0.04);
+						amount = 0.04;
 					else if (enemy.Enemy && enemy.Enemy.tags && enemy.Enemy.tags.has("miniboss"))
-						KinkyDungeonChangeFactionRep(faction, -0.02);
+						amount = 0.02;
 					else if (enemy.Enemy && enemy.Enemy.tags && enemy.Enemy.tags.has("elite"))
-						KinkyDungeonChangeFactionRep(faction, -0.01);
+						amount = 0.01;
 					if (enemy.Enemy && enemy.Enemy.tags && !enemy.Enemy.tags.has("minor"))
-						KinkyDungeonChangeFactionRep(faction, -0.004);
+						amount = 0.004;
 					if (enemy.Enemy && enemy.Enemy.tags && enemy.Enemy.tags.has("minor") && KDRandom() < 0.33)
-						KinkyDungeonChangeFactionRep(faction, -0.004);
+						amount = 0.004;
+				}
+				if (amount) {
+					KinkyDungeonChangeFactionRep(faction, -amount);
+
+					let boostfactions = [];
+					let hurtfactions = [];
+					for (let e of KinkyDungeonEntities) {
+						let dist = KDistChebyshev(e.x - enemy.x, e.y - enemy.y);
+						if (dist < 10) {
+							let faction2 = KDGetFaction(e);
+							if (!KinkyDungeonHiddenFactions.includes(faction2)) {
+								if (KDFactionRelation(faction, faction2) < -0.1 && !boostfactions.includes(faction2)) {
+									boostfactions.push(faction2);
+									KinkyDungeonChangeFactionRep(faction2, 0.5 * amount * -KDFactionRelation(faction, faction2));
+								} else
+								if (KDFactionRelation(faction, faction2) > 0.1 && !hurtfactions.includes(faction2)) {
+									hurtfactions.push(faction2);
+									KinkyDungeonChangeFactionRep(faction2, 0.5 * amount * -KDFactionRelation(faction, faction2));
+								}
+							}
+						}
+					}
 				}
 			} else if (!enemy.summoned) {
 				if (!KDGameData.RespawnQueue) KDGameData.RespawnQueue = [];
