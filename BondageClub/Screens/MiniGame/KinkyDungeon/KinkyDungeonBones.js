2623e99e450f3035749162575f1ca1c4aaa6873c
Ada <gubertpol0@gmail.com>
2022-06-13 23:08:41 -0700

    Patrons

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonBones.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeonBones.js
@@ -10,6 +10,14 @@ let KDPatronAdventurers = [
 let KDPatronCustomEnemies = new Map([
 	["BlindZombie", [
 		//{name: "Your Name Here", color: "#ff7777", prisoner: true, free: true, customSprite: ""},
-	]
+	],
+	],
+	["Wolfgirl", [
+		{name: "Sivasa", color: "#9c2a70", prisoner: false, free: true, customSprite: ""},
+	],
+	],
+	["Alchemist", [
+		{name: "Morgan", color: "#6241e1", prisoner: true, free: true, customSprite: ""},
+	],
 	],
 ]);
